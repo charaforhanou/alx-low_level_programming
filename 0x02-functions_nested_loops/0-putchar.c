59a9fd93c57e8093bab93d63c6c1b02ce0723330
charaforhanou <achraforhanou321@gmail.com>
2023-07-18 19:35:33 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
-
-int main(void) {
-
-/* This program uses the putchar() function to print each character of _putchar followed by a newline character. The return 0; 
+#include "main.h "
+/* This program uses the putchar() function to print each character of _putchar followed by a newline character. The return 0;
  */
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n')
 
+int main(void)
+{
 
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
-return(0);
+	return(0);
 
 }
