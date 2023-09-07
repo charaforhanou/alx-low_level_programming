1897c75bcfb86a8f355394837cd1ac068fe74cd4
charaforhanou <charaforhanou@gmail.com>
2023-09-07 08:40:52 -0700

    ho

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -20,7 +20,7 @@ void close_elf(int elf);
 /**
  * check_elf - Checks if a file is an ELF file.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
- *
+ * user charaf
  * Description: If the file is not an ELF file - exit code 98.
  */
 void check_elf(unsigned char *e_ident)
