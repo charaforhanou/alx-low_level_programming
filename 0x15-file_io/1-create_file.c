d434d998b51be419e9901107597787109a916213
charaforhanou <charaforhanou@gmail.com>
2023-11-27 14:06:44 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,34 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio>
 
-/*
- *Create a function that creates a file.
- *Prototype: int create_file(const char *filename, char *text_content);
- *where filename is the name of the file to create and text_content is a NULL terminated string to write to the file
- *Returns: 1 on success, -1 on failure (file can not be created, file can not be written, write “fails”, etc…)
- *The created file must have those permissions: rw-------. If the file already exists, do not change the permissions.
- *if the file already exists, truncate it
- *if filename is NULL return -1
- *if text_content is NULL create an empty file
+/**
+ * create_file - Crée un fichier.
+ * @filename: Un pointeur vers le nom du fichier à créer.
+ * @text_content: Un pointeur vers une chaîne de caractères à écrire dans le fichier.
+ *
+ * Retourne: Si la fonction échoue - -1.
+ *           Sinon - 1.
  */
- 
 int create_file(const char *filename, char *text_content)
 {
-	int df,len = 0, w;
+	int df, wh, ln = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (ln = 0; text_content[ln];)
+			ln++;
 	}
 
 	df = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(df, text_content, len);
+	wh = write(df, text_content, ln);
 
-	if (df == -1 || w == -1)
+	if (df == -1 || wh == -1)
 		return (-1);
 
 	close(df);
 
 	return (1);
-
 }
+
